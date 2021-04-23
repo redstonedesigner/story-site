0dc65fdf6ab24182470795529657168057fbf317
redstonedesigner <redstonedesigner1@gmail.com>
2021-04-23 17:42:04 +0100

    models: Add category mechanics to story model

--- models/Category.py
+++ models/Category.py
@@ -6,7 +6,8 @@ class Category(Base):
     __tablename__ = "categories"
     id = Column(Integer, primary_key=True, autoincrement=True, unique=True)
     name = Column(String(128), nullable=False, unique=True)
-    short_name = Column(String(4), nullable=False, unique=True)
+    description = Column(String(255), nullable=False)
+    url_slug = Column(String(255), nullable=False, unique=True)
 
     def __init__(self, name=None, short_name=None):
         self.name = name
--- models/Story.py
+++ models/Story.py
@@ -4,16 +4,20 @@ from models.User import User
 
 
 class Story(Base):
-    __tablename__ = "stories"
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    author = Column(String(32), ForeignKey('users.id'), nullable=False)
-    title = Column(String(255), nullable=False)
-    description = Column(String(255), nullable=False)
+	__tablename__ = "stories"
+	id = Column(Integer, primary_key=True, autoincrement=True)
+	author = Column(String(32), ForeignKey('users.id'), nullable=False)
+	title = Column(String(255), nullable=False)
+	description = Column(String(255), nullable=False)
+	categories = Column(String(255), nullable=False)
 
-    def __init__(self, author: User = None, title=None, description=None):
-        self.author = author.id
-        self.description = description
-        self.title = title
+	def __init__(self, author: User = None, title=None, description=None, categories: list = None):
+		self.author = author.id
+		self.description = description
+		self.title = title
+		self.categories = list()
+		for i in categories:
+			self.categories.append(i.id)
 
-    def __repr__(self):
-        return '<User %r>' % self.username
+	def __repr__(self):
+		return '<User %r>' % self.username
