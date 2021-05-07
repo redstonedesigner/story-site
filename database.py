be136a035cb6e0e37016b9086f982dd7d659f973
redstonedesigner <redstonedesigner1@gmail.com>
2021-05-07 16:27:16 +0100

    feat: Implement automated naming convention for keys
    Implements automatic naming conventions for SQLAlchemy in order to reduce errors on database migrations

--- database.py
+++ database.py
@@ -1,11 +1,24 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, MetaData
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
 
 engine = create_engine('sqlite:///database.db', convert_unicode=True)
 db_session = scoped_session(sessionmaker(autocommit=False,
-                                         autoflush=False,
-                                         bind=engine))
-Base = declarative_base()
+										 autoflush=False,
+										 bind=engine))
+
+naming_convention = {
+	"ix": "ix_%(column_0_label)s",
+	"uq": "uq_%(table_name)s_%(column_0_name)s",
+	"ck": "ck_%(table_name)s_%(constraint_name)s",
+	"fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+	"pk": "pk_%(table_name)s"
+}
+
+metadata = MetaData(
+	naming_convention=naming_convention
+)
+
+Base = declarative_base(metadata=metadata)
 Base.query = db_session.query_property()
 import models
